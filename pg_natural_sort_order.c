8fce612ee80d0a526a7a90e9dec3391017ec918e
e-pavlichenko <e-pavlichenko@it-serv.ru>
2024-08-23 05:57:43 +0300

    PG16 support

--- pg_natural_sort_order.c
+++ pg_natural_sort_order.c
@@ -102,22 +102,22 @@ natural_sort_order( PG_FUNCTION_ARGS ) {
     // the non digit.
     // If you find a non-digit and there is no numeric simply add it and continue to the
     // next character.
-    originalData = VARDATA(original);
+    originalData = VARDATA_ANY(original);
     for(i = 0; i < originallen && j < OUTPUT_BUFFER_LENGTH; i++) { 
         if(isdigit(originalData[i]) && k < numericSize) {
             numericBuffer[k++] = originalData[i];
         } else if(k>0) {
-            j = normalizeNumeric(VARDATA(outputBuffer), numericBuffer, j, k, numericSize);
+            j = normalizeNumeric(VARDATA_ANY(outputBuffer), numericBuffer, j, k, numericSize);
             k = 0;
-            VARDATA(outputBuffer)[j++] = originalData[i];
+            VARDATA_ANY(outputBuffer)[j++] = originalData[i];
         } else {
-            VARDATA(outputBuffer)[j++] = originalData[i];
+            VARDATA_ANY(outputBuffer)[j++] = originalData[i];
         }
     }
 
     // Edge case in which there is a number at the end of the original string.
     if(k>0) {
-        j = normalizeNumeric(VARDATA(outputBuffer), numericBuffer, j, k, numericSize);
+        j = normalizeNumeric(VARDATA_ANY(outputBuffer), numericBuffer, j, k, numericSize);
     }
 
     // The size of the string we generated. Don't forget the variable header size.
